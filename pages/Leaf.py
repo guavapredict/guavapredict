4a8531b0b747bc94d7eeca39d463be0531862d2c
guavapredict <134356655+guavapredict@users.noreply.github.com>
2023-05-23 12:09:38 +0530

    Update and rename ♨️.py to Leaf.py

--- pages/Leaf.py
+++ pages/Leaf.py
@@ -5,20 +5,19 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 
-model = tf.keras.models.load_model("saved_model/dryness.hdf5")
+model = tf.keras.models.load_model("saved_model/leaf.hdf5")
 ### load file
 uploaded_file = st.file_uploader("Choose a image file")
 
-map_dict = {0:'Half Dry1',
-            1:'Toodryturmeric',
-            2:'Not',
-            3:'Half Dry2',
-            4:'Driedturmeric'
+map_dict = {0:'Canker',
+            1:'Dot',
+            2:'Mummification',
+            3:'Rust'
             }
 
 
 if uploaded_file is not None:
-    # Convert the file to an opencv image.
+    # Convert the file to an Rust image.
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
     opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
